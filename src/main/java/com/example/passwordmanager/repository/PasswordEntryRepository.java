f8fd5868e6fa2d295f882ca7c1ec98f009f42987
frommthi <frommthi@students.zhaw.ch>
2025-07-01 10:32:14 +0200

    Refactoring

--- src/main/java/com/example/passwordmanager/repository/PasswordEntryRepository.java
+++
@@ -1,9 +0,0 @@
-
-package com.example.passwordmanager.repository;
-
-import com.example.passwordmanager.entity.PasswordEntry;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-// Repository für PasswordEntry. übernimmt alle Standard-Datenbankzugriffe automatisch
-public interface PasswordEntryRepository extends JpaRepository<PasswordEntry, Long> {
-}
